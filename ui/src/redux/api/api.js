dac494b8cc2ba319baaf0802e6a5d6acb061c0bf
Abdulhakeem <dev.abdulhakeem@gmail.com>
2023-07-16 08:10:20 +0100

    Update api.js

--- ui/src/redux/api/api.js
+++ ui/src/redux/api/api.js
@@ -1,3 +1,7 @@
-const API = "http://localhost:3001/api/v1";
+// Production Server
+const API = "https://mernify-xlb0.onrender.com/api/v1"
+
+// Development Server
+// const API = "http://localhost:3001/api/v1";
 
 export default API;
